6f0494bdcbd18a47e135b380cf9f1981d1723225
Lucas Clark <lucas.clark@berkeley.edu>
2024-08-04 22:32:40 -0700

    initial commit - implement Compose

---
+++ src/lib.rs
@@ -0,0 +1,50 @@
+use chrono::Local;
+use edit::{self, edit_file};
+use std::fs;
+use std::{io::Write, path::PathBuf};
+
+pub fn compose(memos_dir: PathBuf, title: Option<String>) -> Result<(), std::io::Error> {
+    // dbg!("in compose", memos_dir, title);
+    let (mut file, filepath) = edit::Builder::new()
+        .suffix(".md")
+        .rand_bytes(5)
+        .tempfile_in(&memos_dir)?
+        .keep()?;
+    // dbg!(&filepath);
+
+    let template = match title.as_deref() {
+        Some(k) => format!("# {k}\n"),
+        None => String::new(),
+    };
+
+    file.write_all(template.as_bytes())?;
+    edit_file(&filepath)?;
+
+    // Use current time as title if no title specified
+    let mut filename = title.unwrap_or(
+        Local::now()
+            .format(
+                //TODO: add date format as a parameter
+                "%b %d, %Y - %I:%M%P",
+            )
+            .to_string(),
+    );
+    // dbg!(&filename);
+
+    for attempt in 0.. {
+        if attempt != 0 {
+            let extension = format!(" ({})", attempt);
+            filename += &extension;
+        }
+        let mut dest = memos_dir.join(&filename);
+        dest.set_extension("md");
+        if dest.exists() {
+            continue;
+        }
+        fs::rename(&filepath, &dest)?;
+        break;
+        // dbg!(&dest);
+    }
+
+    Ok(())
+}
---
+++ src/main.rs
@@ -0,0 +1,96 @@
+use clap::{CommandFactory, Parser, Subcommand};
+use directories::UserDirs;
+use std::fs::create_dir;
+use std::path::PathBuf;
+use std::process::exit;
+
+/// A CLI for jotting down your thoughts.
+#[derive(Parser, Debug)]
+#[clap(version)]
+struct Args {
+    #[clap(short = 'p', long, env)]
+    memo_path: Option<PathBuf>,
+    #[command(subcommand)]
+    cmd: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    /// create a new memo in your text editor
+    ///
+    /// This command will open your $EDITOR, wait for you
+    /// to write something, and then save the file
+    Compose {
+        /// Optionally, a title for the memo
+        ///
+        /// Used as the filename for the memo.
+        /// If no title is supplied, the filename
+        /// will be set to the current time.
+        #[clap(short, long)]
+        title: Option<String>,
+    },
+    /// pick a memo to edit from a list
+    ///
+    /// Use arrow keys to navigate and enter to select
+    /// the memo to open in your $EDITOR
+    Edit {
+        /// Include to open a random memo
+        #[clap(short, long)]
+        random: bool,
+    },
+}
+
+/// Get the user's default memos directory
+/// Placed in their home directory by default
+fn get_default_memos_dir() -> Option<PathBuf> {
+    UserDirs::new().map(|dirs| dirs.home_dir().join(".memos"))
+}
+
+/// Prompt the user for an answer to a yes or no question
+/// Returns true for "y" or "Y", false for anything else.
+fn ask_confirm(question: &str) -> bool {
+    println!("{} (Y/n)", question);
+    let mut input = String::new();
+    match std::io::stdin().read_line(&mut input) {
+        Ok(_) => return (input == "y\n") || (input == "Y\n"),
+        _ => return false,
+    }
+}
+
+fn main() -> Result<(), std::io::Error> {
+    let args = Args::parse();
+    // dbg!(&args);
+
+    // Validate memos dir
+    let Some(memos_dir) = args.memo_path.or_else(get_default_memos_dir) else {
+        let mut cmd = Args::command();
+        cmd.error(
+            clap::error::ErrorKind::ValueValidation,
+            format!("memo directory not provided and home directory not available for memo path creation"),
+        )
+        .exit()
+    };
+    if !memos_dir.exists() {
+        println!("memos directory `{}` does not exist.", memos_dir.display());
+        if !ask_confirm("Would you like to create a new directory there?") {
+            exit(1);
+        }
+        if let Err(e) = create_dir(&memos_dir) {
+            eprintln!("Failed to create memos directory: {e}");
+            exit(1);
+        }
+    }
+    // dbg!(&memos_dir);
+
+    // Execute Command
+    match args.cmd {
+        Commands::Compose { title } => {
+            // dbg!(&title);
+            memo::compose(memos_dir, title)
+        }
+        Commands::Edit { random: _ } => {
+            //TODO: Implement Edit
+            todo!("Implement edit command.");
+        }
+    }
+}
